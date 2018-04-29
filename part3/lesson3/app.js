7f84a21566350de2f285dc37eee5d056871a6342
zxk <zhangxkmail@163.com>
2018-04-29 13:49:41 +0800

    add par3lesson3

---
+++ part3/lesson3/app.js
@@ -0,0 +1,8 @@
+// import 'babel-polyfill'
+let func = () => {};
+const a = 1;
+let arr = [1,2,3]
+arr.map(item => item*2);
+arr.includes(3)
+new Set(1);
+function *fn(){}
---
+++ part3/lesson3/config.part3.lesson3.1.js
@@ -0,0 +1,34 @@
+let path = require('path');
+module.exports = {
+  entry: {
+    app: './app'
+  },
+
+  output: {
+    filename: '[name].[hash:5].js',
+    path: path.resolve(__dirname, './dist1')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        // use: 'babel-loader'  //use 可以直接是一个loader,也可以是一个对象， 也可以是一个数组
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['babel-preset-env', {
+                targets: {
+                  browsers: ['>1%', 'last 2 versions']
+                }
+              }]
+            ]
+          }
+        }
+      }
+
+    ]
+  }
+
+}
